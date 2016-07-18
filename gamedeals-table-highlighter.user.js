57ec218ea2a0c47ae6f9e6088ae9f35708b358fc
Mike Manger <ohmanger@gmail.com>
2016-07-18 14:18:20 +0100

    Bump version to 1.1.3

--- gamedeals-table-highlighter.user.js
+++ gamedeals-table-highlighter.user.js
@@ -6,7 +6,7 @@
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js
 // @updateURL   https://raw.githubusercontent.com/ohmanger/userscripts/master/gamedeals-table-highlighter.user.js
 // @downloadURL https://raw.githubusercontent.com/ohmanger/userscripts/master/gamedeals-table-highlighter.user.js
-// @version     1.1.2
+// @version     1.1.3
 // ==/UserScript==
 
 this.$ = this.jQuery = jQuery.noConflict(true);

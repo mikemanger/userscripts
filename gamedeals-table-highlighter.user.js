c0b01bb77a79a8220d641a94a9eb3ba70e59a2ac
Mike Manger <ohmanger@gmail.com>
2017-05-05 11:02:03 +0100

    Update jQuery to 3.2.1 slim bump version

--- gamedeals-table-highlighter.user.js
+++ gamedeals-table-highlighter.user.js
@@ -3,10 +3,10 @@
 // @namespace   ohmanger
 // @grant       GM_xmlhttpRequest
 // @include     https://www.reddit.com/r/GameDeals/comments/*
-// @require     https://code.jquery.com/jquery-3.1.1.slim.min.js
+// @require     https://code.jquery.com/jquery-3.2.1.slim.min.js
 // @updateURL   https://raw.githubusercontent.com/ohmanger/userscripts/master/gamedeals-table-highlighter.user.js
 // @downloadURL https://raw.githubusercontent.com/ohmanger/userscripts/master/gamedeals-table-highlighter.user.js
-// @version     1.1.8
+// @version     1.1.9
 // ==/UserScript==
 
 this.$ = this.jQuery = jQuery.noConflict(true);

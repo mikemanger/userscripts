146d71a63ee63c40c5f489988a2e4f6fd95cd96c
Mike Manger <ohmanger@gmail.com>
2018-01-22 10:49:09 +0000

    Add FPL Iwon scroller script

---
+++ fpl-iwon-scroller.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name        FPL Iwon scroller
+// @namespace   mikemanger
+// @grant       none
+// @include     https://fplcomps.iwn.io/*/*
+// @updateURL   https://raw.githubusercontent.com/mikemanger/userscripts/master/fpl-iwon-scroller.user.js
+// @downloadURL https://raw.githubusercontent.com/mikemanger/userscripts/master/fpl-iwon-scroller.user.js
+// @version     1.0.0
+// ==/UserScript==
+
+function auto_scrolling() {
+	window.scrollTo(0, document.body.scrollHeight);
+}
+setInterval(auto_scrolling, 500);

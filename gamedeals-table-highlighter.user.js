bd761cfb9caf2347799aba4a7e56bb9aaaad6194
Mike Manger <ohmanger@gmail.com>
2018-07-02 16:14:10 +0100

    Better library loading, support RES darkmode
    get_steam_apps() code taken from https://github.com/raymondlaw/enhanced-fanatical

--- gamedeals-table-highlighter.user.js
+++ gamedeals-table-highlighter.user.js
@@ -6,7 +6,7 @@
 // @require     https://code.jquery.com/jquery-3.3.1.slim.min.js
 // @updateURL   https://raw.githubusercontent.com/mikemanger/userscripts/master/gamedeals-table-highlighter.user.js
 // @downloadURL https://raw.githubusercontent.com/mikemanger/userscripts/master/gamedeals-table-highlighter.user.js
-// @version     1.2.0
+// @version     1.3.0
 // @run-at      document.idle
 // ==/UserScript==
 
@@ -32,40 +32,55 @@ $( users.join(',') ).each( function() {
 
 });
 
-$( '.sitetable' ).on( 'click', 'button.gdth-get-status', function( event ) {
-	event.preventDefault();
 
-	var table_rows = $( this ).siblings( 'table' ).find( 'tbody tr' );
+$(document).ready(function(){
+  main();
+});
 
-	table_rows.each( function() {
-		var tr = $(this),
-		    app_url = tr.html().match( /http(s)?:\/\/store\.steampowered\.com\/app\/(\d*)\//ig );
+async function main() {
+	var user_apps = await get_steam_apps();
 
-		// Skip if no steam URL found
-		if ( app_url === null ) {
-			return 'continue';
-		}
+	$( '.sitetable' ).on( 'click', 'button.gdth-get-status', function( event ) {
+		event.preventDefault();
 
-		GM_xmlhttpRequest({
-			method:  'GET',
-			url:     app_url[0],
-			onload:  function( response ) {
-				var is_logged_in = response.responseText.indexOf( 'Logout();' ) > -1;
-				if ( is_logged_in ) {
-					var in_library = response.responseText.indexOf( '<div class="already_in_library">' ) > -1;
+		var table_rows = $( this ).siblings( 'table' ).find( 'tbody tr' );
 
-					if ( in_library ) {
-						tr.css( 'background', 'lightgreen' );
-					} else {
-						var in_wishlist = response.responseText.indexOf( '<div id="add_to_wishlist_area_success"' ) === -1;
-						if ( in_wishlist ) {
-							tr.css( 'background', 'lightblue' );
-						} else {
-							tr.css( 'background', '' );
-						}
-					}
+		table_rows.each( function() {
+			var tr = $(this),
+				app_url = tr.html().match( /http(s)?:\/\/store\.steampowered\.com\/app\/(\d*)\//i );
+
+			// Skip if no steam URL found
+			if ( app_url === null ) {
+				return 'continue';
+			}
+			var app_id = parseInt( app_url[2] );
+
+			var in_library = user_apps.rgOwnedApps.indexOf( app_id ) > -1;
+
+			if ( in_library ) {
+				tr.css( 'background', 'lightgreen' );
+			} else {
+			  var in_wishlist = user_apps.rgWishlist.indexOf( app_id ) > -1;
+				if ( in_wishlist ) {
+					tr.css( 'background', '#5DADE2' );
+				} else {
+					tr.css( 'background', '' );
 				}
+			}
+
+		});
+	});
+}
+
+function get_steam_apps() {
+	return new Promise(resolve => {
+		GM_xmlhttpRequest({
+			method: 'GET',
+			url: 'https://store.steampowered.com/dynamicstore/userdata/',
+			responseType: 'json',
+			onload: function(response) {
+				resolve( JSON.parse( response.responseText ) );
 			}
 		});
 	});
-});
+}

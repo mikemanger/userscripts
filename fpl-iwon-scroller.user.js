07f6cb3dc55d0e615d1eceddd21364a2d4473e7d
Mike Manger <ohmanger@gmail.com>
2018-07-06 10:31:36 +0100

    Add clicker to fpl script

--- fpl-iwon-scroller.user.js
+++ fpl-iwon-scroller.user.js
@@ -5,10 +5,14 @@
 // @include     https://fplcomps.iwn.io/*/*
 // @updateURL   https://raw.githubusercontent.com/mikemanger/userscripts/master/fpl-iwon-scroller.user.js
 // @downloadURL https://raw.githubusercontent.com/mikemanger/userscripts/master/fpl-iwon-scroller.user.js
-// @version     1.0.0
+// @version     1.0.1
 // ==/UserScript==
 
 function auto_scrolling() {
 	window.scrollTo(0, document.body.scrollHeight);
 }
 setInterval(auto_scrolling, 500);
+
+jQuery(document).ready(function(){
+	jQuery('.submitButton').click();
+});

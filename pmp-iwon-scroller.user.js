a7647bb8bdcae78fde2a91f273e7fa3e18c7bdd8
Mike Manger <ohmanger@gmail.com>
2018-10-02 10:42:01 +0100

    Rename FPL scroller to PMP scroller

---
+++ pmp-iwon-scroller.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        PMP Iwon scroller
+// @namespace   mikemanger
+// @grant       none
+// @include     https://pmpcomps.iwn.io/*/*
+// @updateURL   https://raw.githubusercontent.com/mikemanger/userscripts/master/pmp-iwon-scroller.user.js
+// @downloadURL https://raw.githubusercontent.com/mikemanger/userscripts/master/pmp-iwon-scroller.user.js
+// @version     2.0.0
+// ==/UserScript==
+
+jQuery(document).ready(function(){
+	jQuery('.submitButton').click();
+});

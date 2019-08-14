203ff42a91b0ee39d24c3cf6debbfdca56107ff4
Mike Manger <ohmanger@gmail.com>
2019-08-14 11:46:47 +0100

    Update PMP url

--- pmp-iwon-scroller.user.js
+++ pmp-iwon-scroller.user.js
@@ -2,10 +2,10 @@
 // @name        PMP Iwon scroller
 // @namespace   mikemanger
 // @grant       none
-// @include     https://pmpcomps.iwn.io/*/*
+// @include     https://pmpcomps.iwcomps.com/*/*
 // @updateURL   https://raw.githubusercontent.com/mikemanger/userscripts/master/pmp-iwon-scroller.user.js
 // @downloadURL https://raw.githubusercontent.com/mikemanger/userscripts/master/pmp-iwon-scroller.user.js
-// @version     2.0.0
+// @version     2.0.1
 // ==/UserScript==
 
 jQuery(document).ready(function(){
